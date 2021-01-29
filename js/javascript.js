7e1549c5aa89d52a16743273239b29e5a9632945
Cristhian Galvis <cristiangalviscardona@gmail.com>
2021-01-29 07:58:11 -0500

    Modificaciones al javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,60 +1,67 @@
 
-/* swal("BIENVENIDOS");
-alert("BIENBENIDOS"); */
-/*console.log("Otra manera de generar una alerta");*/
+//alert("Bienvenidos");
+//swal("Bienvenidos");
 
-/*esta funcion me va a generar un nuevo valor del los botones seleccionados*/
+//console.log("PRUEBA HOLA MUNDO");
+
 
 function updateInput(newValue, clear = false) {
-    var div = document.getElementById('date');
-    
-    /* las comillas invertidas se sacan con la tecla altgr + cierrra corchetes*/
-    div.value = clear ? newValue : `${div.value}${newVlaue}`;
- 
-   /* if(clear){
-        div = newValue;
-    }
-    else{
-        div = `${div}${newVlaue}`;
-    } */
+    var div = document.getElementById('data');
+
+    div.value = clear ? newValue : `${div.value}${newValue}`;
+
+    /*if(clear){
+        div.value = newValue;
+    }else{
+        div.value = `${div.value}${newValue}`
+
+        // div.value + newValue
+    }*/
+
 }
 
-function printOperate(date) {
-    updateInput( `${date.value}` );   
+function printOperator(data) {
+    updateInput(` ${data.value} `);
+
+    //' ' + data.value + ' '
 }
-function printnumber(date) {
-    updateInput(date.value);   
+
+function printNumber(data) {
+    updateInput(data.value);
 }
-function ClearInput() {
-    updateInput('' , true);   
+
+function clearInput() {
+    updateInput('', true);
 }
+
 function printOperation() {
-    var date = document.getElementById('date').value;
-    var array = validateOperate (date.split(' '));
-    updateInput(array , true);
+    var data = document.getElementById('data').value;
+    var array = validateOperator(data.split(' '));
+    updateInput(array, true);
 }
 
-function validateOperate (arrayDate) {
+function validateOperator(arrayData) {
     var resultado;
-    var numero1, numero2;
+    var numero1,numero2;
 
-    numero1 = Number(arrayDate[0]);
-    numero2 = Number(arrayDate[2]);
+    numero1 = Number(arrayData[0]);
+    numero2 = Number(arrayData[2]);
 
-    switch (arrayDate) {
+    switch (arrayData[1]) {
         case '+':
             resultado = numero1 + numero2;
             break;
-         case '-':
+        case '-':
             resultado = numero1 - numero2;
             break;
         case '*':
-            resultado = numero1 * numero2;
+            resultado = numero1 * numero2; 
             break;
         case '/':
             resultado = numero1 / numero2;
             break;
-    }
+    }  
 
     return resultado;
+
 }
